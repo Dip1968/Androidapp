267b35fd263d4c86e90d278f599832c2875880d5
Dip <87477818+Dip1968@users.noreply.github.com>
2026-09-21 23:17:07 +0530

    Add MainActivity with basic UI setup

---
+++ app/src/main/java/com/dip1968/androidapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.dip1968.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+
+        text.setText("Androidapp\n\nBuild successful!");
+        text.setTextSize(24);
+        text.setTextColor(Color.BLACK);
+        text.setGravity(Gravity.CENTER);
+        text.setPadding(32, 32, 32, 32);
+
+        setContentView(text);
+    }
+}
